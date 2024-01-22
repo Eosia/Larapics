57bad3aaea3a154a217a711eecb07a9aae565fec
Eosia <kevin.eosiaweb@gmail.com>
2024-01-22 23:22:00 +0100

    Fixation du bug encode intervention image v3

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -4,65 +4,53 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\PhotoRequest;
-
 use Intervention\Image\ImageManager;
 use Intervention\Image\Drivers\Gd\Driver;
-
-
-use App\Models\{
-    Album,
-    Photo,
-    User,
-    Source,
-    Tag,
-};
-
+use App\Models\{ Album, Photo, User, Source, Tag };
 use DB, Storage, Str, Mail;
 use Dotenv\Exception\ValidationException;
 
-$manager = new ImageManager(new Driver());
 class PhotoController extends Controller
 {
-    // ajout de photo à un album
+    // Ajout de photo à un album
     public function create(Album $album) {
         abort_if($album->user_id !== auth()->id(), 403);
 
         $data = [
-            'title' =>  $description = 'Ajouter des photos à ' . $album->title,
+            'title' => $description = 'Ajouter des photos à ' . $album->title,
             'description' => $description,
             'album' => $album,
             'heading' => $album->title,
         ];
         return view('photo.create', $data);
-
     }
 
     public function store(PhotoRequest $request, Album $album) {
         abort_if($album->user_id !== auth()->id(), 403);
         DB::beginTransaction();
-        //$manager = new ImageManager(new Driver());
 
         try {
             $photo = $album->photos()->create($request->validated());
 
             $tags = explode(',', $request->tags);
-
-            $tags = collect($tags)->filter(function($value, $key){
+            $tags = collect($tags)->filter(function($value, $key) {
                 return $value != ' ';
             })->all();
 
-            foreach($tags as $t){
+            foreach ($tags as $t) {
                 $tag = Tag::firstOrCreate(['name' => trim($t)]);
                 $photo->tags()->attach($tag->id);
             }
+
             if ($request->file('photo')->isValid()) {
                 $ext = $request->file('photo')->extension();
                 $filename = Str::uuid() . '.' . $ext;
 
-                $originalPath = $request->file('photo')->storeAs('photos/' . $photo->album_id, $filename);
-                $originalWidth = (int) \Image::read($request->file('photo'))->width();
-                $originalHeight = (int) \Image::read($request->file('photo'))->height();
-                
+                $originalPath = $request->file('photo')->storeAs('photos/' . $album->album_id, $filename);
+                $manager = new ImageManager(new Driver());
+                $image = $manager->read($request->file('photo')->getRealPath());
+                $originalWidth = (int) $image->width();
+                $originalHeight = (int) $image->height();
 
                 $originalSource = $photo->sources()->create([
                     'path' => $originalPath,
@@ -71,11 +59,30 @@ class PhotoController extends Controller
                     'width' => $originalWidth,
                     'height' => $originalHeight,
                 ]);
-                
-            }
 
-        } 
-        catch(ValidationException $e) {
+                $thumbnailImage = $image->resize(350, 233, function($constraint) {
+                    $constraint->aspectRatioCrop();
+                    $constraint->upsize();
+                });
+
+                // Choisissez la méthode d'encodage en fonction de l'extension
+                $encodedThumbnail = match ($ext) {
+                    'png' => $thumbnailImage->toPng(),
+                    'gif' => $thumbnailImage->toGif(),
+                    'bmp' => $thumbnailImage->toBitmap(),
+                    'webp' => $thumbnailImage->toWebp(),
+                    'avif' => $thumbnailImage->toAvif(),
+                    default => $thumbnailImage->toJpeg(),
+                };
+
+                $thumbnailPath = 'photos/'.$album->album_id.'/thumbnails/'.$filename;
+                Storage::put($thumbnailPath, (string) $encodedThumbnail);
+
+                $photo->thumbnail_path = $thumbnailPath;
+                $photo->thumbnail_url = Storage::url($thumbnailPath);
+                $photo->save();
+            }
+        } catch(ValidationException $e) {
             DB::rollBack();
             dd($e->getErrors());
         }
@@ -87,5 +94,4 @@ class PhotoController extends Controller
 
         return redirect($redirect)->withSuccess($success);
     }
-
 }
